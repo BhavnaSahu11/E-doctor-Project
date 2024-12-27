a66ed62529d02d8089a2defce385f2504c3d7f46
unknown <bhavnasahu210226@acropolis.in>
2024-12-27 22:43:13 +0530

    admin module

---
+++ Backend/src/main/java/Outpatient/example/Intership_Backend/Controller/AdminController.java
@@ -0,0 +1,101 @@
+package Outpatient.example.Intership_Backend.Controller;
+
+import Outpatient.example.Intership_Backend.Entity.Doctor;
+import Outpatient.example.Intership_Backend.Entity.Patient;
+import Outpatient.example.Intership_Backend.Service.AdminService;
+import Outpatient.example.Intership_Backend.Service.DoctorService;
+import Outpatient.example.Intership_Backend.Service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/admin/")
+public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+
+    @GetMapping("/doctors")
+    public long getDoctorsCount() {
+        return adminService.getDoctorsCount();
+    }
+
+    @GetMapping("/patients")
+    public long getPatientsCount() {
+        return adminService.getPatientsCount();
+    }
+
+    @GetMapping("/get-welcome-email")
+    public ResponseEntity<Map<String, String>> getWelcomeEmail() {
+        Map<String, String> response = new HashMap<>();
+        response.put("email", adminService.getLoginEmail());
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<String> deleteDoctor(@RequestParam String email) {
+        try {
+            boolean isDeleted = adminService.deleteDoctorByEmail(email);
+            System.out.println(isDeleted);
+            if (isDeleted) {
+                return ResponseEntity.ok("Doctor deleted successfully.");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Doctor not found with email: " + email);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting doctor.");
+        }
+    }
+
+
+    @DeleteMapping("/delete-patient")
+    public ResponseEntity<String> deletePatient(@RequestParam String email) {
+        try {
+            boolean isDeleted = adminService.deletePatientByEmail(email);
+            System.out.println(isDeleted);
+            if (isDeleted) {
+                return ResponseEntity.ok("Patient deleted successfully.");
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Patient not found with email: " + email);
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error deleting doctor.");
+        }
+    }
+
+    //admin by me
+    @PostMapping("/add-doctor")
+    public ResponseEntity<String> addDoctor(@RequestBody Doctor doctor) {
+        try {
+            boolean isAdded = adminService.addDoctor(doctor);
+            if (isAdded) {
+                return ResponseEntity.status(HttpStatus.CREATED).body("Doctor added successfully.");
+            } else {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Doctor with email already exists.");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error adding doctor.");
+        }
+    }
+
+    @PostMapping("/add-patient")
+    public ResponseEntity<String> addPatient(@RequestBody Patient patient) {
+        try {
+            boolean isAdded = adminService.addPatient(patient);
+            if (isAdded) {
+                return ResponseEntity.status(HttpStatus.CREATED).body("Patient added successfully.");
+            } else {
+                return ResponseEntity.status(HttpStatus.CONFLICT).body("Patient with email already exists.");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error adding patient.");
+        }
+    }
+
+}
